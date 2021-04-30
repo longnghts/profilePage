4a55524c1b96dc6c6491fbc55407f5e9bf192232
longnghts <82634370+longnghts@users.noreply.github.com>
2021-04-29 21:18:50 -0400

    added changes for "Making Connections" assignment

---
+++ script.js
@@ -0,0 +1,40 @@
+function editName() {
+  document.getElementById('name').innerHTML = "Mitchell Jenkins";
+}
+
+function hideRequest1() {
+  var hide= document.getElementById('toddRequest');
+  hide.style.display = "none";
+
+}
+
+function hideRequest2() {
+  var hide= document.getElementById('philRequest');
+  hide.style.display = "none";
+
+}
+
+function hideRequest3() {
+  var hide= document.getElementById('adrienRequest');
+  hide.style.display = "none";
+
+}
+
+function hideRequest4() {
+  var hide= document.getElementById('anneRequest');
+  hide.style.display = "none";
+
+}
+
+function hideRequest5() {
+  var hide= document.getElementById('alayneRequest');
+  hide.style.display = "none";
+
+}
+
+function hideRequest6() {
+  var hide= document.getElementById('arryRequest');
+  hide.style.display = "none";
+
+}
+
